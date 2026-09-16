c69c38437c191dca80549c3d52c8141e8888881b
madx030318 <110534918+madx030318@users.noreply.github.com>
2026-09-16 23:44:47 +0500

    Update Project1.h
    The implementation of data functions to call in cpp file

--- Project1.h
+++ Project1.h
@@ -1,1 +1,24 @@
+#ifndef REGISTRATIONSYSTEM_H
+#define REGISTRATIONSYSTEM_H
 
+using namespace std;
+
+class RegistrationSystem
+{
+private:
+
+    string DatabaseFile;
+
+public:
+
+    RegistrationSystem();
+
+    RegistrationSystem(string FileName);
+
+    void RegisterClient(IdentificationSystem& Client);
+
+    void AddClientToDatabase(const IdentificationSystem& Client);
+
+    bool AccountExists(int AccountNumber);
+
+};
