69740bb5e13e96109670a33e0f2a3ac79bd53cf3
madx030318 <110534918+madx030318@users.noreply.github.com>
2026-09-17 18:59:04 +0500

    Update CreditManagementprofile.h
    Applying initialdata for credit system for further impleemantion to bankinginterface.cpp as service client can use

--- CreditManagementprofile.h
+++ CreditManagementprofile.h
@@ -4,3 +4,38 @@
 
 #include <string>
 #include "DatabaseClient.h"
+
+using namespace std;
+
+class CreditSystem {
+private:
+int AccountNumber;
+bool Application;
+int SumofCredit;
+int PercentOfYear;
+string ApplicationDate;
+string EndingDate;
+bool ApprovedStatus;
+int DurationMonths;
+public:
+
+    CreditSystem();
+
+    CreditSystem(
+        int AccountNumber,
+        int Sum,
+        int PercentOfYear,
+        string ApplicationDate,
+        string EndingDate,
+        int DurationMonths
+    );
+
+void ApplyForCredit();
+bool CanBeApproved();
+void AnalyzeEndingDate();
+void DisplayCreditInformation();
+int FindClient();
+bool IsApproved();
+}
+
+#endif
